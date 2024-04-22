22458073f4f05c8243fabd63183f4a742fe3bb99
Piotr Pośpiech <piootrp01@gmail.com>
2024-04-22 12:46:46 +0200

    fix filterUniqueWords in task2

--- Task2.js
+++ Task2.js
@@ -2,15 +2,17 @@ const getFullName = person => `${person.firstName} ${person.lastName}`
 
 
 const changeToLowerCase = string => string.toLowerCase() // changing the whole text to lower case
-const splitText = string => string.split(' ')  // changing the text into an array of words
+const match = string =>  string.match(/\b(?![\w\d]*\d)[\w]+/g)   // filtering only words in text (without special characters or numbers) and changing the whole text to array of words
 const removeDuplicates = array => array.filter((value, i, self) => { // removing duplicates from an array 
     return i === self.indexOf(value);
 });
 const sortArray = array => array.sort() // sorting the array
-const filterUniqueWords = text => sortArray(removeDuplicates(splitText(changeToLowerCase(text))))
+const filterUniqueWords = text => sortArray(removeDuplicates(match(changeToLowerCase(text))))
     
 const getAverageGrade = (students) => {
     const getGrades = array => array.reduce((accumulator, student) => accumulator + student.grade,0)    // getting the sum of the grades
     return getGrades(students)/students.length
 }
+
+
 
